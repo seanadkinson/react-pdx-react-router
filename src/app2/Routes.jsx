5bb56db1bd409f40d777c7b6cd0f062dd0bb57d0
Sean Adkinson <sean.adkinson@gmail.com>
2015-02-08 22:08:44 -0800

    Slight changes

--- src/app2/Routes.jsx
+++ src/app2/Routes.jsx
@@ -4,7 +4,7 @@ var React = require('react'),
     Router = require('react-router');
 
 module.exports = (
-    <Router.Route path="/" handler={require('./AppPage.jsx')}>
+    <Router.Route handler={require('./AppPage.jsx')}>
         <Router.Route name="home" path="/" handler={require('./HomePage.jsx')} />
         <Router.Route name="beer" handler={require('./BeerPage.jsx')} />
         <Router.Route name="menu" handler={require('./MenuPage.jsx')} />
