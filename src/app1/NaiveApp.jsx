0f5a5fb838a75a977467244aad668059eb14722b
Sean Adkinson <sean.adkinson@gmail.com>
2015-02-08 21:32:34 -0800

    Header

--- src/app1/NaiveApp.jsx
+++ src/app1/NaiveApp.jsx
@@ -17,18 +17,27 @@ var Application = React.createClass({
     render: function() {
         return (
             <div className="container">
+
+                <header className="row">
+                    <h1>React Restaurant</h1>
+                </header>
+
                 <div className="row">
                     <div className="col-xs-12" role="tabpanel">
                         <ul className="nav nav-tabs">
+
                             <li role="presentation" className={this.state.page == 'home' ? 'active' : ''}>
                                 <a href="" onClick={this.showPage.bind(this, 'home')}>Home</a>
                             </li>
+
                             <li role="presentation" className={this.state.page == 'beer' ? 'active' : ''}>
                                 <a href="" onClick={this.showPage.bind(this, 'beer')}>Beer</a>
                             </li>
+
                             <li role="presentation" className={this.state.page == 'menu' ? 'active' : ''}>
                                 <a href="" onClick={this.showPage.bind(this, 'menu')}>Menu</a>
                             </li>
+
                         </ul>
                         <div className="tab-content">
                             {this.renderPage()}
@@ -54,7 +63,7 @@ var HomePage = React.createClass({
     render: function() {
         return (
             <div>
-                <h1>Home Page</h1>
+                <h3>Home Page</h3>
                 This is the home page
             </div>
         );
@@ -65,7 +74,7 @@ var BeerPage = React.createClass({
     render: function() {
         return (
             <div>
-                <h1>Beer Page</h1>
+                <h3>Beer Page</h3>
                 Check out our wide variety of beers to choose from.
             </div>
         );
@@ -76,7 +85,7 @@ var MenuPage = React.createClass({
     render: function() {
         return (
             <div>
-                <h1>Menu Page</h1>
+                <h3>Menu Page</h3>
                 Look at our nice menu of food to eat.
             </div>
         );
