542a45978a26cc0e7e572779aa7fc824b24b4c31
Sean Adkinson <sean.adkinson@gmail.com>
2015-02-10 13:40:38 -0800

    Added comments on routes

--- src/app2/Routes.jsx
+++ src/app2/Routes.jsx
@@ -5,9 +5,16 @@ var React = require('react'),
 
 module.exports = (
     <Router.Route handler={require('./AppPage.jsx')}>
+
+        {/*  path: /  */}
         <Router.Route name="home" path="/" handler={require('./HomePage.jsx')} />
+
+        {/*  path: /menu  */}
         <Router.Route name="menu" handler={require('./MenuPage.jsx')} />
+
+        {/*  path: /contact  */}
         <Router.Route name="contact" handler={require('./ContactPage.jsx')} />
+
     </Router.Route>
 );
 
--- src/app3/Routes.jsx
+++ src/app3/Routes.jsx
@@ -6,16 +6,26 @@ var React = require('react'),
 module.exports = (
     <Router.Route path="/app/3" handler={require('./AppPage.jsx')}>
 
+        {/*  path: /  */}
         <Router.DefaultRoute name="home" handler={require('./HomePage.jsx')}/>
 
+        {/*  path: /contact  */}
         <Router.Route name="contact" handler={require('./ContactPage.jsx')}/>
 
         <Router.Route name="menu" handler={require('./MenuPage.jsx')}>
+
+            {/*  path: /menu  */}
             <Router.DefaultRoute name="menu-index" handler={require('./MenuIndex.jsx')}/>
+
+            {/*  path: /menu/123  */}
             <Router.Route name="menu-item" path=":itemId" handler={require('./MenuItem.jsx')}/>
+
         </Router.Route>
 
+        {/*  path: /404  */}
         <Router.Route name="404" handler={require('./NotFound.jsx')} />
+
+        {/*  path: /not-exsts  */}
         <Router.NotFoundRoute handler={require('./NotFound.jsx')} />
 
     </Router.Route>
